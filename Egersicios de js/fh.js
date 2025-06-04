5d144706850edc81b1f3b7113c29d44dc002b2be
neikerde404 <chirinosneiker373@gmail.com>
2025-06-04 16:54:29 -0400

    commitp

---
+++ Egersicios de js/fh.js
@@ -0,0 +1,49 @@
+let edad = 20
+
+if (edad < 18) {
+    console.long("Menor de edad");
+} else if (edad >= 18 && edad < 65) {
+    console.long("Adulto");
+} else {
+    ("Adulto mayor");console.log
+}
+
+let dia = 3;
+let nombreDia;
+
+switch (dia) {
+    case 1:
+        nombreDia = "lunes";
+        break;
+    case 2:
+        nombreDia = "Martes";
+        break;
+    case 3:
+        nombreDia = "Miercoles";
+        break;
+    case 4:
+        nombreDia = "Jueves";
+    case 5:
+        nombreDia = "Viernes";
+        break;
+    case 6:
+        nombreDia ="Sabado";
+        break;
+    case 7:
+        nombreDia ="Domingo";
+        break;
+    default:
+        nombreDia ="Dia invalido";
+}
+console.long 
+
+for (let i = 0; i < 5; i++) {
+    console.log(i); // Imprime 0, 1, 2, 3, 4
+}
+
+let i = 0;
+while (i = 5) {
+    console.log(i); // Imprime o, 1, 2, 3, 4,
+}
+
+ 
